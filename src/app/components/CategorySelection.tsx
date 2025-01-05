2284773eb1d5ba02c5b0a411062edb7f897bc9be
Areesha-sites <areesha21314@gmail.com>
2025-01-05 14:58:55 +0500

    updated

--- src/app/components/CategorySelection.tsx
+++ src/app/components/CategorySelection.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import { CategorySelectionTypes } from "../../../types/componentTypes";
 import { CategorySelectionPropTypes } from "../../../types/componentTypes";
 import Image from "next/image";
+
 const CategorySelection = ({
   onSelectCategory,
   activeCategory,
 }: CategorySelectionPropTypes) => {
   const categories: CategorySelectionTypes[] = [
-    { name: "All", bgColor: "#E0E0E0", imgSrc: "/category8.jpg" },
-    { name: "Lunch", bgColor: "#FFEBCC", imgSrc: "/category7.jpg" },
-    { name: "Breakfast", bgColor: "#E0F7FA", imgSrc: "/category6.jpg" },
-    { name: "Beverages", bgColor: "#FCE4EC", imgSrc: "/category5.jpg" },
-    { name: "Vegetarian", bgColor: "#FFF9C4", imgSrc: "/category4.jpg" },
-    { name: "Snacks", bgColor: "#D1C4E9", imgSrc: "/category3.jpg" },
+    { name: "All", bgColor: "#E0E0E0", imgSrc: "/category1.jpg" },
+    { name: "Lunch", bgColor: "#FFEBCC", imgSrc: "/category2.jpg" },
+    { name: "Breakfast", bgColor: "#E0F7FA", imgSrc: "/category3.jpg" },
+    { name: "Beverages", bgColor: "#FCE4EC", imgSrc: "/category4.jpg" },
+    { name: "Vegetarian", bgColor: "#FFF9C4", imgSrc: "/category5.jpg" },
+    { name: "Snacks", bgColor: "#D1C4E9", imgSrc: "/category6.jpg" },
     {
       name: "Quick Meals",
       bgColor: "#C8E6C9",
-      imgSrc: "/category2.jpg",
+      imgSrc: "/category7.jpg",
     },
-    { name: "Healthy Food", bgColor: "#FFE0B2", imgSrc: "/category1.jpg" },
+    { name: "Healthy Food", bgColor: "#FFE0B2", imgSrc: "/category8.jpg" },
   ];
 
   return (
@@ -39,9 +40,11 @@ const CategorySelection = ({
           <Image
             src={category.imgSrc}
             alt={category.name}
-            height={20}
-            width={20}
-            className="w-5 h-5 rounded-full mr-2"
+            height={20} 
+            width={20}  
+            priority
+            quality={100} 
+            className="w-5 h-5 rounded-full mr-2 object-cover" 
           />
           {category.name}
         </button>
